2eb98f2fb83e4e28901ddeb4064a262950f6409c
sergiosan118 <sergis13@ucm.es>
2024-04-25 00:17:58 +0200

    mostrar icono notificacion, problema, la imagen da error

--- app/src/main/java/com/example/myapplication/utils/adapters/ConsultasListAdapter.java
+++ app/src/main/java/com/example/myapplication/utils/adapters/ConsultasListAdapter.java
@@ -1,9 +1,11 @@
 package com.example.myapplication.utils.adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.myapplication.activities.ChatViewActivity;
 import com.example.myapplication.R;
 import com.example.myapplication.data.Consulta;
+import com.example.myapplication.utils.Controller;
 import com.example.myapplication.utils.manager.NavigationManager;
 
 import java.util.LinkedList;
@@ -21,10 +24,12 @@ public class ConsultasListAdapter extends RecyclerView.Adapter<ConsultasListAdap
     protected LinkedList<Consulta> consultasList;
 
     protected Context context;
+    protected String dni;
     protected View.OnClickListener onClickListener;
-    public ConsultasListAdapter(LinkedList<Consulta> list, Context c){
+    public ConsultasListAdapter(LinkedList<Consulta> list, Context c,String dni_paciente){
         context = c;
         consultasList = list;
+        dni = dni_paciente;
     }
 
     public void setOnItemClickListener(View.OnClickListener onClickListener) {
@@ -36,7 +41,7 @@ public class ConsultasListAdapter extends RecyclerView.Adapter<ConsultasListAdap
     public ConsultasListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.consulta_item, parent, false);
         view.setOnClickListener(onClickListener);
-        return new ViewHolder(view,context);
+        return new ViewHolder(view,context,dni);
     }
 
     @Override
@@ -59,10 +64,17 @@ public class ConsultasListAdapter extends RecyclerView.Adapter<ConsultasListAdap
         public TextView titulo;
         public Context context;
         public Consulta e;
-        public ViewHolder(@NonNull View itemView, Context context) {
+
+        public String dni;
+
+        public ImageView image_notificacion;
+        public ViewHolder(@NonNull View itemView, Context context,String dni) {
             super(itemView);
 
             titulo = itemView.findViewById(R.id.tituloTextView);
+            image_notificacion = itemView.findViewById(R.id.imageViewMensajesNoLeidos);
+
+            this.dni = dni;
 
             view = itemView;
             this.context=context;
@@ -78,6 +90,23 @@ public class ConsultasListAdapter extends RecyclerView.Adapter<ConsultasListAdap
         }
         public void bind(Consulta b){
             e=b;
+
+            boolean flag = Controller.getInstance().getSaberSiHayMensajesNoLeidos(b.getId(),dni);
+            Log.d("FlagValue", "El valor de flag es: " + flag);
+
+
+            //LA IMAGEN NO FUNCIONA Y ME PETA, EL RESTO EN PRINCIPIO VA BIEN
+            if(image_notificacion!=null){
+
+                if (flag){
+                    image_notificacion.setVisibility(View.VISIBLE);
+
+                }else{
+                    image_notificacion.setVisibility(View.GONE);
+                }
+            }
+
+
             if(b.getTitulo().length()>35){
                 titulo.setText(b.getTitulo().substring(0,34)+"...");
             }else{
