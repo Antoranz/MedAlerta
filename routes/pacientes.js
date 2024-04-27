5affb361c97abd37a10898a87b363b35445b5116
sergiosan118 <sergis13@ucm.es>
2024-04-27 20:59:31 +0200

    .

--- routes/pacientes.js
+++ routes/pacientes.js
@@ -41,17 +41,13 @@ router.post('/crearNotificacionCita/:dni', async function(req, res, next) {
         console.log("Dni Doctor:" + dni_doctor[0].DNIDoctor);
 
 
-        const fechaHoraString = fecha + ' ' + hora + ':00'; // Combinar fecha y hora en una sola cadena, en formato ISO 8601 y UTC
-        const nuevoDate = new Date(fechaHoraString);
-        nuevoDate.setHours(nuevoDate.getHours() - 2);
-        nuevoDate.setMonth(nuevoDate.getMonth() - 2);
 
-        console.log(nuevoDate);
-        // Convertir la fecha ajustada a una cadena en formato ISO 8601
-        const fecha_hora = formatISODateWithoutAdjustment(nuevoDate);
+        const fechaHoraString = fecha + ' ' + hora + ':00'; // Combinar fecha y hora en una sola cadena, en formato ISO 8601 y UTC
+        
+       
 
         console.log(fechaHoraString);
-        console.log(fecha_hora);
+       
 
 
         await dao.crearNotificacionCita(tipo,motivo,fechaHoraString,dni_doctor[0].DNIDoctor,dni_paciente);
@@ -65,21 +61,6 @@ router.post('/crearNotificacionCita/:dni', async function(req, res, next) {
       }
    
 });
-
-function formatISODateWithoutAdjustment(date) {
-    // Obtener partes de la fecha
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
-    const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
-  
-    // Formatear la fecha sin ajustar a UTC
-    return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`;
-  }
-
 
 
 router.get('/obtenerAlarmas/:dni', async function(req, res, next) {
