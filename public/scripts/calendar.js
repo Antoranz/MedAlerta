8993c147b217e3e8c8085498383f25444273ec6c
Antoranz <frantora@ucm.es>
2024-05-15 11:52:06 +0200

    Arreglo calendario

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -1,36 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth', // Vista inicial: mensual
-        slotDuration: '00:15:00', // Duración de las casillas del calendario en minutos (en este caso, 15 minutos)
-        slotLabelInterval: '00:15:00', // Intervalo para mostrar etiquetas de tiempo (en este caso, 15 minutos)
+        initialView: 'dayGridMonth', //Vista inicial: mensual
+        slotDuration: '00:15:00', //Duración de las casillas del calendario en minutos (en este caso, 15 minutos)
+        slotLabelInterval: '00:15:00', //Intervalo para mostrar etiquetas de tiempo (en este caso, 15 minutos)
         businessHours: {
-            // Horario laboral
+            //Horario laboral
             daysOfWeek: [1, 2, 3, 4, 5],
-            startTime: '08:00', 
-            endTime: '17:00' 
+            startTime: '12:00', 
+            endTime: '20:00' 
         },
-        selectable: true, 
         selectConstraint: 'businessHours', 
-        selectOverlap: false, 
-        selectAllow: function(selectInfo) {
-            var duration = moment.duration(selectInfo.end.diff(selectInfo.start));
-            return duration.asMinutes() === 15;
-        },
         headerToolbar: {
-            left: 'prev,next today', 
-            center: 'title', 
-            right: 'dayGridMonth,timeGridWeek,timeGridDay' // Botones para alternar vistas
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
-        dateClick: function(info) {
-            // Manejador de evento clic en una fecha
-            calendar.gotoDate(info.dateStr); // Cambiar a la vista del día clickeado
-            calendar.changeView('timeGridDay'); // Cambiar la vista al día
-        }
     });
     calendar.on('eventClick', function(info) {
         var evento = info.event;
-        // Crear contenido del modal con información del evento
         var modalContent = `
             <div class="modal-content">
                 <div class="modal-header">
@@ -48,36 +36,43 @@ document.addEventListener('DOMContentLoaded', function() {
                 </div>
             </div>
         `;
-        var modalElement = document.getElementById('myModal'); // Obtener el elemento del modal por su id
-        var modalBody = modalElement.querySelector('.modal-body'); // Obtener el cuerpo del modal dentro del elemento modal
-        modalBody.innerHTML = modalContent; // Establecer el contenido en el cuerpo del modal
-        var modal = new bootstrap.Modal(modalElement); // Crear la instancia del modal con el elemento obtenido
-        modal.show(); // Mostrar el modal
+        var modalElement = document.getElementById('myModal'); 
+        var modalBody = modalElement.querySelector('.modal-body'); 
+        modalBody.innerHTML = modalContent; 
+        var modal = new bootstrap.Modal(modalElement); 
+        modal.show(); 
         
-        
-        // Agregar manejador de clic para el botón de eliminar cita
         document.getElementById('eliminarCitaBtn').addEventListener('click', function() {
-            eliminarCita(evento.id); // Llamar a la función para eliminar la cita
+            eliminarCita(evento.id); 
             modal.hide();
         });
     });
     cargarCitas();
+    calendarEl.addEventListener('click', cambiarAVistaDiaria);
     calendar.render();
     function eliminarCita(eventoId) {
         $.ajax({
-            url: '/doctor/citas/eliminar-cita', // Ruta en el servidor para eliminar la cita
-            type: 'POST', // Método HTTP utilizado
-            data: { id: eventoId }, // Datos a enviar al servidor (el identificador del evento)
+            url: '/doctor/citas/eliminar-cita', 
+            type: 'POST',
+            data: { id: eventoId },
             success: function(response) {
                 calendar.removeAllEvents();
                 cargarCitas();
                 console.log('Cita eliminada con éxito');
-                // Aquí puedes agregar cualquier otra acción que desees después de eliminar la cita
             },
             error: function(xhr, status, error) {
                 console.error('Error al eliminar la cita:', error);
             }
         });
+    }
+    function cambiarAVistaDiaria(event) {
+        var tdElement = event.target.closest('.fc-day');
+    
+        if (tdElement && tdElement.dataset.date) {
+            var fechaClickeada = tdElement.dataset.date;
+            calendar.gotoDate(fechaClickeada); 
+            calendar.changeView('timeGridDay');
+        }
     }
     function cargarCitas(){
         $.ajax({
@@ -107,4 +102,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
-
